eeef0c6e9dccffd3b7fc9d794ec75bc2b528287e
mrs1669 <mrs1669.develop@gmail.com>
2020-02-29 20:54:01 +0900

    shade

--- Mario/Assets/Scripts/PlayerController.cs
+++ Mario/Assets/Scripts/PlayerController.cs
@@ -4,21 +4,34 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private RectTransform player;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GetComponent<RectTransform>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
-        this.transform.position += new Vector3(0, -2, 0);
         if (Input.GetKey(KeyCode.Space))
         {
             this.transform.position += new Vector3(0, 3, 0);
         }
+        else
+        {
+            if(player.position.y > 0)
+            {
+                this.transform.position += new Vector3(-2, -2, 0);
+                if(player.position.y <= 0)
+                {
+                    player.transform.position = new Vector3(0, 0);
+                }
+            }
+        }
 
+        Debug.Log(player.position.x);
     }
 }
