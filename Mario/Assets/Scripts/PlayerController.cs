a0eb3bf2e580c61f38ffbce60feb3bf5533299b3
mrs1669 <mrs1669.develop@gmail.com>
2020-03-01 00:20:24 +0900

    add Crouching

--- Mario/Assets/Scripts/PlayerController.cs
+++ Mario/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     private RectTransform player;
     private int playerX, playerY; // プレイヤーのX座標,Y座標 左下が(0,0)
     private int playerDy; // プレイヤーのY座標加速度
+    private int playerWidth, playerHeight;
     private int groundY; // プレイヤーの高さを考慮した地面の座標設定 
 
     // Start is called before the first frame update
@@ -16,7 +17,9 @@ public class PlayerController : MonoBehaviour
         playerX = 300;
         playerY = 300;
         playerDy = 0;
-        groundY = Mathf.FloorToInt(GetComponent<RectTransform>().sizeDelta.y)/2;
+        playerHeight = Mathf.FloorToInt(GetComponent<RectTransform>().sizeDelta.y);
+        playerWidth = Mathf.FloorToInt(GetComponent<RectTransform>().sizeDelta.x);
+        groundY = playerHeight / 2;
     }
 
     // Update is called once per frame
@@ -58,7 +61,8 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKey(KeyCode.A))
         {
             playerX += -15;
-        }else if (Input.GetKey(KeyCode.D))
+        }
+        else if (Input.GetKey(KeyCode.D))
         {
             playerX += 15;
         }
@@ -66,6 +70,23 @@ public class PlayerController : MonoBehaviour
         {
             playerX += 0;
         }
+
+        // しゃがみ機能
+        if (Input.GetKey(KeyCode.S))
+        {
+            GetComponent<RectTransform>().sizeDelta = new Vector2(playerWidth, playerHeight / 2);
+            groundY = Mathf.FloorToInt(GetComponent<RectTransform>().sizeDelta.y) / 2;
+        }
+        else
+        {
+            GetComponent<RectTransform>().sizeDelta = new Vector2(playerWidth, playerHeight);
+            groundY = Mathf.FloorToInt(GetComponent<RectTransform>().sizeDelta.y) / 2;
+            if (playerY < groundY)
+            {
+                playerY = groundY;
+            }
+        }
+
     }
 
 }
