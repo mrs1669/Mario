9b4cf0a669eb313fae5014561e840085782752dd
mrs1669 <mrs1669.develop@gmail.com>
2020-02-29 21:08:55 +0900

    ground attachment

--- Mario/Assets/Scripts/PlayerController.cs
+++ Mario/Assets/Scripts/PlayerController.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private RectTransform player;
+    private int playerX, playerY; // プレイヤのX座標,Y座標 左下が(0,0)
 
     // Start is called before the first frame update
     void Start()
     {
         player = GetComponent<RectTransform>();
+        playerX = 300;
+        playerY = 300;
     }
 
     // Update is called once per frame
@@ -18,20 +21,23 @@ public class PlayerController : MonoBehaviour
         
         if (Input.GetKey(KeyCode.Space))
         {
-            this.transform.position += new Vector3(0, 3, 0);
+            playerX += 3;
         }
         else
         {
             if(player.position.y > 0)
             {
-                this.transform.position += new Vector3(-2, -2, 0);
-                if(player.position.y <= 0)
+                playerX += 2;
+                playerY += -2;
+                if (playerY <= 0)
                 {
-                    player.transform.position = new Vector3(0, 0);
+                    playerY = 0;
                 }
             }
         }
 
-        Debug.Log(player.position.x);
+        this.transform.position = new Vector3(playerX, playerY, 0);
+
+        Debug.Log(player.position.y);
     }
 }
