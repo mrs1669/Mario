2e6f0124cffa30c5db5a026686e39aebdb522d7e
mrs1669 <mrs1669.develop@gmail.com>
2020-02-29 15:18:56 +0900

    first commit

---
+++ Mario/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        this.transform.position += new Vector3(0, -2, 0);
+        if (Input.GetKey(KeyCode.Space))
+        {
+            this.transform.position += new Vector3(0, 3, 0);
+        }
+
+    }
+}
